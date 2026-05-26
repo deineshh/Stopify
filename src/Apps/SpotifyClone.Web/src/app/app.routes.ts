b34492356b8bbb9fdeca9995fa00b10f47783f99
deinesh <tramboladenes@gmail.com>
2026-05-26 16:42:52 +0300

    feat: create Spotify.Web Angular project

---
+++ src/Apps/SpotifyClone.Web/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { RenderMode } from '@angular/ssr';
+
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     component: LandingPageComponent,
+    //     data: { renderMode: RenderMode.Prerender } 
+    // }
+];
